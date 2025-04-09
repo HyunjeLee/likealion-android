2ca88bca7c6efe727604661ed06035429415daa8
HyunjeLee <cyeclamen07@gmail.com>
2025-04-09 14:19:47 +0900

    feat(ch8_sec2): menu & SearchView

---
+++ ch8/src/main/java/com/example/ch8/sec2/Test2_1Activity.java
@@ -0,0 +1,83 @@
+package com.example.ch8.sec2;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.ch8.R;
+
+public class Test2_1Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+
+        setContentView(R.layout.activity_test21);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+
+    // 액티비티의 메뉴 구성 함수  // 액티비티 초기화하며 한 번 호출됨
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        menu.add(0, 0, 0, "menu1");
+//        menu.add(0,1,0,"menu2");
+
+        // 위처럼 직접 메뉴 객체에 add하지 않고 xml에서 추가 후 뷰를 출력만해도 메뉴는 화면에 출력됨
+        getMenuInflater().inflate(R.menu.menu_test2, menu);
+        // 하지만 searchView 추가 시
+        MenuItem menuItem = menu.findItem(R.id.menu3);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
+        searchView.setQueryHint("hint");
+        searchView.setOnQueryTextListener(onQueryTextListener);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // 메뉴 이벤트 함수  // 인자는 현재 이벤트가 발생 메뉴 아이템 객체
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu1) {
+            Toast.makeText(this, "menu1 click", Toast.LENGTH_SHORT)
+                    .show();
+        } else if (item.getItemId() == R.id.menu2) {
+            Toast.makeText(this, "menu2 click", Toast.LENGTH_SHORT)
+                    .show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    SearchView.OnQueryTextListener onQueryTextListener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            Toast.makeText(Test2_1Activity.this, query, Toast.LENGTH_SHORT).show();
+
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            return false;
+        }
+    };
+
+}
