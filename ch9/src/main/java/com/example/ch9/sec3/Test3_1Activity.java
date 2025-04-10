a6e7416d26c58fbbbb6d9765b6bf0d3c41bcbac9
HyunjeLee <cyeclamen07@gmail.com>
2025-04-10 17:52:52 +0900

    refactor(ch9_sec3): intent 관련 클릭 리스너 구현 방식 변경
    기존 각각의 익명 내부 클래스 구현에서
    하나의 내부 클래스 생성으로 구현 방식 변경

--- ch9/src/main/java/com/example/ch9/sec3/Test3_1Activity.java
+++ ch9/src/main/java/com/example/ch9/sec3/Test3_1Activity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -40,35 +41,55 @@ public class Test3_1Activity extends AppCompatActivity {
         ActivityResultLauncher<String> launcher = registerForActivityResult(
                 new ActivityResultContracts.RequestPermission(),
                 isGranted -> {
-                    if(isGranted){
-                        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-120"));
+                    if (isGranted) {
+                        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-999-9999"));
                         startActivity(intent);
-                    }else {
+                    } else {
                         Toast.makeText(this, "permission denied..", Toast.LENGTH_SHORT)
                                 .show();
                     }
                 }
         );
 
-        binding.btnCall.setOnClickListener(view -> {
-            if(ContextCompat.checkSelfPermission(this, "android.permission.CALL_PHONE")
-                    == PackageManager.PERMISSION_GRANTED){
-                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-120"));
-                startActivity(intent);
-            }else {
-                launcher.launch("android.permission.CALL_PHONE");
+        MyClickListener myClickListener = new MyClickListener(launcher);
+
+        binding.btnCall.setOnClickListener(myClickListener);
+        binding.btnMap.setOnClickListener(myClickListener);
+        binding.btnBrowser.setOnClickListener(myClickListener);
+    }
+
+
+    private class MyClickListener implements View.OnClickListener {
+
+        private final ActivityResultLauncher<String> launcher;
+
+        private MyClickListener(ActivityResultLauncher<String> launcher) {
+            this.launcher = launcher;
+        }
+
+        @Override
+        public void onClick(View v) {
+            int id = v.getId();
+
+            if (id == R.id.btn_call) {
+                if (ContextCompat.checkSelfPermission(Test3_1Activity.this, "android.permission.CALL_PHONE")
+                        == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-999-9999"));
+                    startActivity(intent);
+                } else {
+                    launcher.launch("android.permission.CALL_PHONE");
+                }
             }
+            if (id == R.id.btn_map) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:37.566,127.977"));
+                startActivity(intent);
+            }
+            if (id == R.id.btn_browser) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.naver.com"));
 
-        });
-
-        binding.btnMap.setOnClickListener(view -> {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:37.566,127.977"));
-            startActivity(intent);
-        });
-        binding.btnBrowser.setOnClickListener(view -> {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.naver.com"));
-            startActivity(intent);
-        });
+                startActivity(intent);
+            }
+        }
     }
 
 
